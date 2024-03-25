df287997b14ab785fd1220a38115be22b4f0a60f
cdecompilador <nyagouno@gmail.com>
2024-03-25 20:40:38 +0100

    testing work

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,9 +14,13 @@ dependencies {
 
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
 }
 
 tasks.test {
     useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
 }
